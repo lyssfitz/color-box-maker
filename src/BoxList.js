6b5eeb9cd6926f63b94c79c4d98f1045b2804701
Lyss <lyssfitz@outlook.com>
2020-02-21 17:38:02 +1100

    Add NewBoxForm component

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Box from './Box';
+import NewBoxForm from './NewBoxForm';
 
 class BoxList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { boxes: [{ width: 10, height: 40, color: 'orange' }] };
+		this.create = this.create.bind(this);
+	}
+
+	create(newBox) {
+		this.setState({
+			boxes: [...this.state.boxes, newBox],
+		});
 	}
 	render() {
 		const boxes = this.state.boxes.map(box => <Box width={box.width} height={box.height} color={box.color} />);
@@ -12,6 +20,7 @@ class BoxList extends Component {
 			<div>
 				<h1>Color Box Maker</h1>
 				{boxes}
+				<NewBoxForm createBox={this.create} />
 			</div>
 		);
 	}
